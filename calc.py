0a964273b3385002bc82e1ea883b1a27c4d2ba8d
Corporal <zipzk241_zds@student.ztu.edu.ua>
2025-02-05 23:28:45 +0200

    Update calc.py
    Перевірка на ділення на нуль

--- calc.py
+++ calc.py
@@ -6,7 +6,10 @@ def calculate(num1, num2, operator):
     elif operator == '*':
         return num1 * num2
     elif operator == '/':
-        return num1 / num2
+        if num2 == 0:
+            return "Cannot divide by zero"
+        else:
+            return num1 / num2
     else:
         return "Invalid operator"
 
