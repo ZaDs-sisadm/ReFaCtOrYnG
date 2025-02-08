c8ef616caf9bc25c23e20cc1c8ffb507c2f8a318
vitalii <bondarfamily123@gmail.com>
2025-02-08 14:00:55 +0200

    refactor: solve 'spaghetti' code and implement one layer of abstraction

--- calc.py
+++ calc.py
@@ -7,6 +7,7 @@ def calculate(num1, num2, operator):
         return multiplication_of_two_nums(num1, num2)
     elif operator == '/':
         return division_of_two_nums(num1, num2)
+    #Suggestion:Removal this part of conditional that could shrink this part only for calculation,not to deliberate about possible pitfalls
     else:
         return "Invalid operator"
 
@@ -17,10 +18,8 @@ def division_of_two_nums(num1, num2):
     else:
         return num1 / num2
 
-
 def summa_of_two_nums(num1, num2):
     return num1 + num2
-
 
 def subtraction_of_two_nums(num1, num2):
     return num1 - num2
@@ -29,15 +28,29 @@ def subtraction_of_two_nums(num1, num2):
 def multiplication_of_two_nums(num1, num2):
     return num1 * num2
 
+def main():
+    num1, num2 = couple_nums_validation()
+    operator = operator_logic_implementation()
+    result = calculate(num1, num2, operator)
+    print("Result:", result)
 
-try:
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-except ValueError:
-    print("Invalid input. Please enter a number.")
-    exit()
-    
-operator = input("Enter operator (+, -, *, /): ")
 
-result = calculate(num1, num2, operator)
-print("Result:", result)
+def operator_logic_implementation():
+    operator = input("Enter operator (+, -, *, /): ")
+    # Possible implementation of business logic
+    return operator
+
+
+def couple_nums_validation():
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        exit()
+    return num1, num2
+
+
+if __name__ == "__main__":
+    main()
+
